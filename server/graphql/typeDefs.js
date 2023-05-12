00ce8ffe052b34691ad2ef8e53e31f17f55ce9a3
Juanes-xd <juanestebanortizbejarano@gmail.com>
2023-05-12 18:44:08 -0500

    Models, add and getAll projects

--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -1,7 +1,20 @@
-import {gql} from 'graphql-tag'
+import { gql } from "graphql-tag";
 
 export const typeDefs = gql`
-    type Query{
-        hello: String
-    }
+  type Query {
+    hello: String
+    projects: [Project]
+  }
+
+  type Mutation {
+    createProject(name: String, description: String): Project
+  }
+
+  type Project {
+    _id: ID
+    name: String
+    description: String
+    createdAt: String
+    updatedAt: String
+  }
 `;
