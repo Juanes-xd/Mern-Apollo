337365e01aebd646e3c54a9ae802212e0f2c544a
Juanes-xd <juanestebanortizbejarano@gmail.com>
2023-05-26 13:19:36 -0500

    Project form and create project query

--- client/src/components/ProjectForm.jsx
+++ client/src/components/ProjectForm.jsx
@@ -1,3 +1,52 @@
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { CREATE_PROJECT, GET_PROJECTS } from "../graphql/projects";
+
 export const ProjectForm = () => {
-  return <div>ProjectForm</div>;
+  const [project, setProject] = useState({
+    name: "",
+    description: "",
+  });
+
+  const [createProject, { loading, error }] = useMutation(CREATE_PROJECT, {
+    refetchQueries: [
+      {
+        query: GET_PROJECTS,
+      },
+      "GetProjects",
+    ],
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setProject({
+      ...project,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createProject({
+      variables: {
+        name: project.name,
+        description: project.description,
+      },
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {error && <p>{error.message}</p>}
+      <input
+        type="text"
+        name="name"
+        placeholder="Write a name"
+        onChange={handleChange}
+      />
+      <textarea name="description" rows="3" onChange={handleChange} />
+      <button disabled={!project.name || !project.description || loading}>
+        Save
+      </button>
+    </form>
+  );
 };
